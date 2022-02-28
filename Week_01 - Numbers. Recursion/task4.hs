5a302e31c043a9b4cc4bf848caab2bd76c81e288
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-02-28 10:55:46 +0200

    Difference in speed between iterative and recursive processes.

--- Week_01 - Numbers. Recursion/task4.hs
+++ Week_01 - Numbers. Recursion/task4.hs
@@ -4,15 +4,18 @@ main = do
     print $ fibIter 11 == 89
     print $ fibIter 110 == 43566776258854844738105
 
-fibRec :: Int -> Int
+    -- Be careful! The following line will crash you PC!
+    -- print $ fibRec 110 == 43566776258854844738105
+
+fibRec :: Integer -> Integer
 fibRec 0 = 0
 fibRec 1 = 1
 fibRec n = fibRec (n - 1) + fibRec (n - 2)
 
-fibIter :: Int -> Integer
+fibIter :: Integer -> Integer
 fibIter n = helper n 0 1
  where
-     helper :: Int -> Integer -> Integer -> Integer
+     helper :: Integer -> Integer -> Integer -> Integer
      helper 0 n1 _ = n1
      helper 1 _ n2 = n2
      helper n n1 n2 = helper (n - 1) n2 (n1 + n2)
