d220941dda5ec8d89127c71f6323e89a9471462b
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-04-09 14:38:28 +0300

    Comment out unnecessary code.

--- Additional_1/task5.hs
+++ Additional_1/task5.hs
@@ -5,10 +5,10 @@ main = do
                     -- [3, 6, 9 ] [3, 4, 5 ]
 
     -- print $ foldl () True xs
-    print $ map f 6
-    print $ map (\ x -> x + 1) [6]
+    -- print $ map f 6
+    -- print $ map (\ x -> x + 1) [6]
 
-f x = x + 1
+-- f x = x + 1
 
 dominatesFold :: (Int -> Int) -> (Int -> Int) -> [Int] -> Bool
 dominatesFold f g = foldl (\ acc x -> f x >= g x) True
