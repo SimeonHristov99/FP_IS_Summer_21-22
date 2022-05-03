77a46859eb0a98d8e8cd648f47ca0c335950dbf1
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-05-03 17:56:13 +0300

    Add solutions.

--- Week_11 - Trees (part 1)/task4.hs
+++ Week_11 - Trees (part 1)/task4.hs
@@ -7,6 +7,17 @@ main = do
     print $ isBinarySearchTree t5 == False
     print $ isBinarySearchTree t6 == False
 
+data BTree = Nil | Node Int BTree BTree
+
+traverseDFS :: BTree -> [Int]
+traverseDFS Nil = []
+traverseDFS (Node value left right) = traverseDFS left ++ [value] ++ traverseDFS right
+
+isBinarySearchTree :: BTree -> Bool
+isBinarySearchTree t = nodes == sort nodes
+ where
+     nodes = traverseDFS t
+
 t3 :: BTree
 t3 = Node 8 (Node 3 (Node 1 Nil Nil) (Node 6 (Node 4 Nil Nil) (Node 7 Nil Nil))) (Node 10 Nil (Node 14 (Node 13 Nil Nil) Nil))
 
