f6034a6c3fe941d55606d55cb7fa79249e50e4f1
SimeonHristov99 <s.e.hristov99@gmail.com>
2022-05-17 17:58:17 +0300

    Add solutions.

--- Week_13 - Preparation for Test 2 and Exam (part 1)/task1.hs
+++ Week_13 - Preparation for Test 2 and Exam (part 1)/task1.hs
@@ -13,6 +13,8 @@ main = do
     print $ containsWord t3 "bdi" == True
     print $ containsWord t3 "ac" == False
 
+data BTree a = Nil | Node a (BTree a) (BTree a) 
+
 t1 :: BTree Char
 t1 = Node 'a' (Node 'c' (Node 'f' Nil Nil) (Node 'd' Nil Nil)) (Node 'b' Nil (Node 'e' Nil Nil))
 
@@ -21,3 +23,30 @@ t2 = Node 'a' (Node 'c' (Node 'd' Nil Nil) Nil) (Node 'b' Nil Nil)
 
 t3 :: BTree Char
 t3 = Node 'a' (Node 'b' (Node 'd' (Node 'h' Nil Nil) (Node 'i' Nil Nil)) (Node 'e' Nil Nil)) (Node 'c' (Node 'f' Nil Nil) (Node 'g' Nil Nil))
+
+containsWord :: (Eq a) => BTree a -> [a] -> Bool
+containsWord Nil [] = True
+containsWord (Node value Nil Nil) [x] = value == x
+containsWord (Node value left right) (x:xs)
+ | value == x = helper left xs || helper right xs
+ | otherwise = containsWord left (x:xs) || containsWord right (x:xs)
+ where
+     helper (Node value Nil Nil) [x] = x == value
+     helper (Node value left right) (x:xs)
+      | value == x = helper left xs || helper right xs
+      | otherwise = False
+     helper _ _ = False
+containsWord _ _ = False
+
+-- containsWord' :: (Eq a) => BTree a -> [a] -> Bool
+-- containsWord' Nil [] = True
+-- containsWord' Nil (x:xs) = False
+-- containsWord' (Node value left right) [] = False
+-- containsWord' (Node value Nil Nil) [x] = value == x
+-- containsWord' (Node value left right) (x:xs)
+--  | value == x = helper left xs || helper right xs
+--  | otherwise = containsWord left (x:xs) || containsWord right (x:xs)
+--  where
+--      helper (Node value Nil Nil) [x] = x == value
+--      helper (Node value left right) (x:xs) = value == x && (helper left xs || helper right xs)
+--      helper _ _ = False
